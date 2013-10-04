edf26598a5757377275b42b612887f0cd0001820
akash <akash@akash.com>
2013-10-04 18:07:23 +0300

    datbase uri changed to heroku

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ MAX_SEARCH_RESULTS = 50
 # pagination
 POSTS_PER_PAGE = 3
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/blog02'
+SQLALCHEMY_DATABASE_URI = 'postgresql://xssvmqvqabqilw:37m-6eL7kd8KjYXRBUNuSnOrTf@ec2-54-247-98-97.eu-west-1.compute.amazonaws.com/blog'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/blog02'
 #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
