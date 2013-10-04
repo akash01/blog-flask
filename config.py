46f0f4e4c336d50be18994b4b744d9dbd228a8b5
akash01 <pace.akash@gmail.com>
2013-10-05 01:58:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,5 @@ MAX_SEARCH_RESULTS = 50
 # pagination
 POSTS_PER_PAGE = 3
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://xssvmqvqabqilw:37m-6eL7kd8KjYXRBUNuSnOrTf@ec2-54-247-98-97.eu-west-1.compute.amazonaws.com/de44tr89hdat81'
-#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/blog02'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/blog02'
 #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
